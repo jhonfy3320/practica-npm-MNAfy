3f43968ab7edf16bcc7f8c9a800d7fdd66246709
jhonfyenaksmpy <ftaveraenmsafy23478@gmail.com>
2025-04-29 18:19:33 -0500

    Todo Agregado

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="practica-npm-MNAfy",                           # Nombre del paquete
+    version="0.1.1",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Jhonfypymna",                         # Tu nombre
+    author_email="freddy_3_t@hotmail.com",                 # Tu correo electrónico
+    url="https://github.com/jhonfy3320/practica-npm-MNAfy",     # URL del proyecto
+)
